2eb634b7b3fc90d695ad51c28e93b2a56c33ac1e
Joe Chen <geometryolife@gmail.com>
2022-02-12 09:50:39 +0800

    handle illegal input

--- Chapter02/guessing_game/src/main.rs
+++ Chapter02/guessing_game/src/main.rs
@@ -17,15 +17,20 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line"); // 调用标准输入句柄的 read_line 方法来获取输入
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!"); // trim 方法去除多余的换行符，parse 方法解析字符串为数值
+        let guess: u32 = match guess.trim().parse() { // trim 方法去除多余的换行符，parse 方法解析字符串为数值
+            Ok(num) => num, // match 表达式匹配合适的分支并执行相应的代码
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) { // 比较猜测数字与保密数字（随机数）
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break; // 猜对后退出循环
+            }
         }
     }
 }
