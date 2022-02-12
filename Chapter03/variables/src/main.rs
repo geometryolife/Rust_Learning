4bfdd409a91e912427ee95a0c09564f10b86b0af
Joe Chen <geometryolife@gmail.com>
2022-02-12 16:10:09 +0800

    bool type

--- Chapter03/variables/src/main.rs
+++ Chapter03/variables/src/main.rs
@@ -1,21 +1,8 @@
 fn main() {
-    // 加法
-    let sum = 5 + 10;
-    println!("{}", sum);
+    // 布尔类型
+    let t = true;
+    println!("{}", t);
 
-    // 减法
-    let difference = 95.5 - 4.3;
-    println!("{}", difference);
-
-    // 乘法
-    let product = 4 * 30;
-    println!("{}", product);
-
-    // 除法
-    let quotient = 56.7 / 32.2;
-    println!("{}", quotient);
-
-    // 取余
-    let remainder = 43 % 5;
-    println!("{}", remainder);
+    let f: bool = false; // 附带了显式类型标注的语句
+    println!("{}", f);
 }
