6e4a48085076bbea392f27cd870dc0af9b99ba81
Joe Chen <geometryolife@gmail.com>
2022-02-12 10:42:38 +0800

    shadow mechanism

--- Chapter03/variables/src/main.rs
+++ Chapter03/variables/src/main.rs
@@ -1,9 +1,4 @@
 fn main() {
-    let x = 5; // 声明变量 x
-
-    let x = x + 1; // 隐藏第一个变量 x
-
-    let x = x * 2; // 隐藏第二个变量 x
-
-    println!("The value of x is: {}", x);
+    let spaces = "   "; // 字符串类型
+    let spaces = spaces.len(); // 数值类型
 }
