e82491745cb6abce89bd6f75609871ccb2b42017
Joe Chen <geometryolife@gmail.com>
2022-02-12 17:05:57 +0800

    access array element

--- Chapter03/variables/src/main.rs
+++ Chapter03/variables/src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
-    // 初始化数组的方式
     let a = [1, 2, 3, 4, 5]; // 初始化数组
 
-    let months = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "Septembe", "October", "November", "December"];
+    // 访问数组元素
+    let first = a[0];
+    let second = a[1];
 
-    let b: [i32; 5] = [1, 2, 3, 4, 5]; // 显式写出数组的类型
-
-    // 创建一个含有相同元素的数组
-    // 等价于 let c = [3, 3, 3, 3, 3];
-    let c = [3; 5];
+    println!("{}\n{}", first, second);
 }
