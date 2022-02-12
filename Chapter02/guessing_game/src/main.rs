f380aea2f2043c7c051a0ddf0e82d79022ac22f4
Joe Chen <geometryolife@gmail.com>
2022-02-12 09:25:39 +0800

    add loop

--- Chapter02/guessing_game/src/main.rs
+++ Chapter02/guessing_game/src/main.rs
@@ -9,21 +9,23 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new(); // 生成空字符串
+        let mut guess = String::new(); // 生成空字符串
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line"); // 调用标准输入句柄的 read_line 方法来获取输入
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line"); // 调用标准输入句柄的 read_line 方法来获取输入
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!"); // trim 方法去除多余的换行符，parse 方法解析字符串为数值
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!"); // trim 方法去除多余的换行符，parse 方法解析字符串为数值
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) { // 比较猜测数字与保密数字（随机数）
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) { // 比较猜测数字与保密数字（随机数）
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
