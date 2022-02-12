5faa83e612924f7be3e7032101465f3632c8ee80
Joe Chen <geometryolife@gmail.com>
2022-02-12 17:02:27 +0800

    array type

--- Chapter03/variables/src/main.rs
+++ Chapter03/variables/src/main.rs
@@ -1,20 +1,13 @@
 fn main() {
-    // 复合类型
-    // 元组类型
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    // 初始化数组的方式
+    let a = [1, 2, 3, 4, 5]; // 初始化数组
 
-    let (x, y, z) = tup; // 解构元组
+    let months = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "Septembe", "October", "November", "December"];
 
-    println!("The value of y is: {}", y);
+    let b: [i32; 5] = [1, 2, 3, 4, 5]; // 显式写出数组的类型
 
-    // 使用点号结构元组
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-
-    let five_hundred = x.0;
-
-    let six_point_four = x.1;
-
-    let one = x.2;
-
-    println!("{} {} {}", five_hundred, six_point_four, one);
+    // 创建一个含有相同元素的数组
+    // 等价于 let c = [3, 3, 3, 3, 3];
+    let c = [3; 5];
 }
