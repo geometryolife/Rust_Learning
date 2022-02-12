24da25731be269e56b126c15c5e0f7059b33c703
Joe Chen <geometryolife@gmail.com>
2022-02-12 17:09:51 +0800

    illegal access array elements

--- Chapter03/variables/src/main.rs
+++ Chapter03/variables/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
     let a = [1, 2, 3, 4, 5]; // 初始化数组
+    let index = 10;
 
-    // 访问数组元素
-    let first = a[0];
-    let second = a[1];
+    let element = a[index]; // 非法的数组访问
 
-    println!("{}\n{}", first, second);
+    println!("The value of element is: {}", element);
+
+    // 注意：旧版本的 rust 能够编译，但是目前我使用的版本无法编译
+    // rustc 1.53.0 (53cb7b09b 2021-06-17)
 }
