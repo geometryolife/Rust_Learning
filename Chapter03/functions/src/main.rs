7d40e9b33e68b561630d8d9dc21c1ef4b4324264
Joe Chen <geometryolife@gmail.com>
2022-02-14 22:31:35 +0800

    multi parameter

--- Chapter03/functions/src/main.rs
+++ Chapter03/functions/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    another_function(5);
+    another_function(5, 6);
 }
 
-fn another_function(x: i32) { // 函数签名必须显式地声明每个参数的类型
+// 声明多个参数
+fn another_function(x: i32, y: i32) { // 函数签名必须显式地声明每个参数的类型
     println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
