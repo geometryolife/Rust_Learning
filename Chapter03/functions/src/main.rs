c6b563612611aa122363163fda27a8f8e70318b1
Joe Chen <geometryolife@gmail.com>
2022-02-14 22:34:46 +0800

    statement

--- Chapter03/functions/src/main.rs
+++ Chapter03/functions/src/main.rs
@@ -1,9 +1,3 @@
 fn main() {
-    another_function(5, 6);
-}
-
-// 声明多个参数
-fn another_function(x: i32, y: i32) { // 函数签名必须显式地声明每个参数的类型
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-}
+    let y = 6; // 语句
+} // 语句
