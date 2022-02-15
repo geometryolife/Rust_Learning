0a1e814ded4aae2b23d9feee489a51fa83f4934b
Joe Chen <geometryolife@gmail.com>
2022-02-15 10:19:04 +0800

    for2

---
+++ Chapter03/loops4/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // 重构例 3-3：使用 for 来完成特定次数的任务
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+//3!
+//2!
+//1!
+//LIFTOFF!!!
