83defb0d2f5da5c11d386c88f879163665fa551c
Joe Chen <geometryolife@gmail.com>
2022-02-15 09:52:27 +0800

    loop2

--- Chapter03/loops/src/main.rs
+++ Chapter03/loops/src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    loop {
-        println!("again!"); // 无限循环
-    }
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2; // 结束并返回值
+        }
+    }; // 结束当前语句
+
+    println!("The result is {}", result);
 }
+
+// The result is 20
