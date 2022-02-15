ccf4a3c209ebf9d1ce1227af929c4dbb062881ac
Joe Chen <geometryolife@gmail.com>
2022-02-15 09:22:19 +0800

    multi else if

--- Chapter03/branches/src/main.rs
+++ Chapter03/branches/src/main.rs
@@ -1,9 +1,18 @@
 fn main() {
-    let number = 3;
+    let number = 6;
 
-    if number != 0 {
-        println!("number was something other than zero");
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
 
-// number was something other than zero
+//   Compiling branches v0.1.0 (/home/ubuntu/other/Rust_Learning/Chapter03/branches)
+//    Finished dev [unoptimized + debuginfo] target(s) in 0.24s
+//     Running `/home/ubuntu/other/Rust_Learning/Chapter03/branches/target/debug/branches`
+//number is divisible by 3
