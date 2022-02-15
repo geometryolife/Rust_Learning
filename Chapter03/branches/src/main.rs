3b702db96508d02f57061958c046c7d9e0e73a39
Joe Chen <geometryolife@gmail.com>
2022-02-15 09:01:44 +0800

    if expression

---
+++ Chapter03/branches/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true"); // 分支（arm）
+    } else {
+        println!("condition was false");
+    }
+}
+
+
+// condition was true
