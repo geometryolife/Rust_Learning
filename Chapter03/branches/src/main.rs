fe741dda127238e62207265ad759f3b87befdb38
Joe Chen <geometryolife@gmail.com>
2022-02-15 09:11:49 +0800

    not equal to zero

--- Chapter03/branches/src/main.rs
+++ Chapter03/branches/src/main.rs
@@ -1,23 +1,9 @@
 fn main() {
-    let number = 7;
+    let number = 3;
 
-    if number { // Rust 不会自动尝试将非布尔类型转换为布尔类型
-        println!("condition was true"); // 分支（arm）
-    } else {
-        println!("condition was false");
+    if number != 0 {
+        println!("number was something other than zero");
     }
 }
 
-//   Compiling branches v0.1.0 (/home/ubuntu/other/Rust_Learning/Chapter03/branches)
-//error[E0308]: mismatched types
-// --> src/main.rs:4:8
-//  |
-//4 |     if number { // Rust 不会自动尝试将非布尔类型转换为布尔类型
-//  |        ^^^^^^ expected `bool`, found integer
-
-//error: aborting due to previous error
-
-//For more information about this error, try `rustc --explain E0308`.
-//error: could not compile `branches`
-
-//To learn more, run the command again with --verbose.
+// number was something other than zero
