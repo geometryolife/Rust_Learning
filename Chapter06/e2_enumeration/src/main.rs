7bd1020f026a6e392e5d196dc8811fc060b961a1
Joe Chen <geometryolife@gmail.com>
2022-02-26 10:01:33 +0800

    use struct to store enum

---
+++ Chapter06/e2_enumeration/src/main.rs
@@ -0,0 +1,22 @@
+// 【例6-1】使用 struct 来存储 IP 地址的数据和 IpAddrKind 变体
+fn main() {
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+}
