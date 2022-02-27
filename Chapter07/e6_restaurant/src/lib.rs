fcd2fe8a39e6eb7cf19f98e4a7377b361eb525a4
Joe Chen <geometryolife@gmail.com>
2022-02-27 16:03:47 +0800

    let enum is pub

---
+++ Chapter07/e6_restaurant/src/lib.rs
@@ -0,0 +1,12 @@
+// 【例7-10】公开一个枚举会同时将它的所有字段公开
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
