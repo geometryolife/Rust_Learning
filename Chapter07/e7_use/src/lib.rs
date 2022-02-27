9d14054acad5af27877c13f6b07d434d0be2bc1a
Joe Chen <geometryolife@gmail.com>
2022-02-27 16:10:48 +0800

    use `use` keyword for absolute path

---
+++ Chapter07/e7_use/src/lib.rs
@@ -0,0 +1,14 @@
+// 【例7-11】使用 use 将模块引入作用域
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
