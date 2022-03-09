e7ccf9837942c1f164b8081488f0c8c9964e6b46
Joe Chen <geometryolife@gmail.com>
2022-03-09 16:10:58 +0800

    match different mistakes

---
+++ Chapter09/e5_open_file/src/main.rs
@@ -0,0 +1,18 @@
+// 【例9-5】用不同的方式处理不同的错误类型
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+            },
+            other_error => panic!("There was a problem opening the file: {:?}", other_error),
+        },
+    };
+}
