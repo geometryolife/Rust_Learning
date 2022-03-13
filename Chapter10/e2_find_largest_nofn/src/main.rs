b9bf3b2e1331c0fa71e149a21edf3d501bf35b48
Joe Chen <geometryolife@gmail.com>
2022-03-13 21:54:32 +0800

    find largest number from two number list respectively

---
+++ Chapter10/e2_find_largest_nofn/src/main.rs
@@ -0,0 +1,30 @@
+// 【例10-2】在两个数字列表中分别找到最大值
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+    // === Output ===
+    // The largest number is 100
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+    // === Output ===
+    // The largest number is 6000
+}
