c7a05fc57998c1e846c4fbf20567b41b37f925d2
Joe Chen <geometryolife@gmail.com>
2022-04-09 18:13:51 +0800

    print errors to standard error

--- Chapter12/e13_minigrep_driven/src/main.rs
+++ Chapter12/e13_minigrep_driven/src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        // println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
 
         process::exit(1);
     });
@@ -16,8 +17,16 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = e13_minigrep_driven::run(config) {
-        println!("Application error: {}", e);
+        // println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
 }
+
+// cargo run > output.txt
+// === Output ===
+// Problem parsing arguments: not enough argument
+
+// 正常情况
+// cargo run to poem.txt > output.txt
