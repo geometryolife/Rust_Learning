8672ba5f8c5e1d8a83d5b8fa6084a5757f024e95
Joe Chen <geometryolife@gmail.com>
2022-04-09 10:23:38 +0800

    write a failing test for `search_case_insensitive` fn

---
+++ Chapter12/e13_minigrep_driven/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::process;
+
+use e13_minigrep_driven::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = e13_minigrep_driven::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
